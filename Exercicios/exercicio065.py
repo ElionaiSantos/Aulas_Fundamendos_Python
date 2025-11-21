5423f9ca4a9fd78d31066f7a0d599e18b1fd513e
ElionaiSantos <106438281+ElionaiSantos@users.noreply.github.com>
2025-11-21 17:16:54 +0000

    ex67

--- Exercicios/exercicio065.py
+++ Exercicios/exercicio065.py
@@ -5,6 +5,24 @@ jogadores = {}
 
 for c in range(qtdJogadores):
     nome = input(f'Digite o nome do {c+1}º jogador: ')
-    jogada = randint( 1, 6)
+    jogada = randint(1, 6)
 
     jogadores[nome] = jogada
+
+auxiliar = jogadores.copy()
+ranking = list()
+
+while len(auxiliar) > 0:
+    maior_jogador = ''
+    maior_valor = 0
+
+    for jogador, jogada in auxiliar.items():
+        if jogada > maior_valor:
+            maior_jogador = jogador
+            maior_valor = jogada
+    ranking.append((maior_jogador, maior_valor))
+    del auxiliar[maior_jogador]
+
+print('\n--- RANKING FINAL ---')
+for i, (nome, valor) in enumerate(ranking):
+    print(f' {i + 1}º Lugar: O jogador {nome} tirou o valor {valor}')
---
+++ Exercicios/exercicio067.py
@@ -0,0 +1,8 @@
+jogador = dict()
+
+jogador['nome'] = input('Digite o nome do jogador: ')
+jogador['Quanditade de jogos'] = int(input('Digite a quantidade de jogos: '))
+jogador['Quantidade de golos'] = int(input('Digite a quantidade de golos: '))
+jogador['Média de jogos'] = jogador['Quantidade de golos'] / jogador['Quantidade de jogos']
+
+
